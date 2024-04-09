807e2b067003cb21d490ec0b5dc5c9a28784b8f5
Saicharan1505 <kothasaicharanreddy@gmail.com>
2024-04-09 19:50:00 -0400

    practicum-13

--- src/main/java/edu/iu/saikotha/primeservice/controller/PrimesController.java
+++ src/main/java/edu/iu/saikotha/primeservice/controller/PrimesController.java
@@ -1,7 +1,10 @@
 package edu.iu.saikotha.primeservice.controller;
 
+import com.nimbusds.jose.proc.SecurityContext;
 import edu.iu.saikotha.primeservice.service.IPrimesService;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 import edu.iu.saikotha.primeservice.rabbitmq.MQSender;
 
@@ -23,9 +26,13 @@ public class PrimesController {
     }
 
     @GetMapping("/{n}")
-    public boolean isPrimeNumber(@PathVariable("n") int n){
+    public boolean isPrimeNumber(@PathVariable int n){
         boolean result = primesService.isPrime(n);
-        mqSender.sendMessage(n,result);
+        Object principal = SecurityContextHolder
+                .getContext().getAuthentication().getPrincipal();
+        String username = ((Jwt) principal).getSubject();
+        System.out.println(username);
+        mqSender.sendMessage(username,n,result);
         return result;
 
     }
